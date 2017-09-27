be7dea7204a62c4a2fa13b340858d038c284f565
Vladimir Bogretsov <bogrecov@gmail.com>
2017-09-27 13:41:04 +0300

    tmp

--- cmd/maild/conf.go
+++ cmd/maild/conf.go
@@ -1,4 +1,4 @@
-package conf
+package main
 
 import (
 	"github.com/urfave/cli"
--- cmd/maild/main.go
+++ cmd/maild/main.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/urfave/cli"
-
-	"github.com/vbogretsov/maild/maild/conf"
-)
-
 const (
 	Name    = "maild"
 	Usage   = "notification service for micro service architecture"
 	Version = "0.0.0"
 )
-
-func run(cfg *conf.Conf) {
-	fmt.Println("maild started with configuration:", cfg)
-}
-
-func createApp() *cli.App {
-	app := cli.NewApp()
-	app.Name = Name
-	app.Usage = Usage
-	app.Version = Version
-	return app
-}
-
-func main() {
-	app := createApp()
-	cfg := conf.New(app)
-
-	app.Action = func(c *cli.Context) {
-		run(cfg)
-	}
-
-	app.Run(os.Args)
-}
